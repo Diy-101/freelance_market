ee1fe7f5266b0bddf5d9d3e361eb766f31e2a7f3
Diy-101 <thehorsediy101@gmail.com>
2025-08-06 14:16:53 +0300

    changed base url to '/'

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,15 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/freelance_market/' : '/',
+  base: "/",
+  // base: process.env.NODE_ENV === 'production' ? '/freelance_market/' : '/',
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
