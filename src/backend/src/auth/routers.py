86522080c3a2d21c4b7b86805071fcffdd8e3820
Diy-101 <thehorsediy101@gmail.com>
2025-08-12 12:56:17 +0300

    refactoring

--- src/backend/src/auth/routers.py
+++ src/backend/src/auth/routers.py
@@ -32,17 +32,17 @@ async def signin(
     # Добавление пользователя в БД
     user = db.execute(select(User).where(User.tg_id == user_data.id)).scalar_one_or_none()
     user_data_dict = user_data.model_dump()
-    if not user:
-        user_data_dict["tg_id"] = user_data_dict.pop("id")
+    user_data_dict["tg_id"] = user_data_dict.pop("id")
 
+    if not user:
         new_user = User(**user_data_dict)
         db.add(new_user)
         db.commit()
         db.refresh(new_user)
 
     user_data_dict["id"] = user_data_dict.pop("tg_id")
-    access_token = authx.create_access_token(uid=user_data["id"])
-    refresh_token = authx.create_refresh_token(uid=user_data["id"])
+    access_token = authx.create_access_token(uid=user_data_dict["id"])
+    refresh_token = authx.create_refresh_token(uid=user_data_dict["id"])
 
     return SignIn(
         access_token=access_token,
