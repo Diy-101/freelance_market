4cb747bcc3d3cfec2223f5da3afd329d33949247
Diy-101 <thehorsediy101@gmail.com>
2025-08-12 14:34:33 +0300

    changed the name

--- src/hooks/useTelegramAuth.ts
+++ src/hooks/useTelegramAuth.ts
@@ -1,5 +1,5 @@
 import { useState } from "react";
-const BASE_URL = import.meta.env.BASE_URL;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const useTelegramAuth = () => {
   const [userData, setUserData] = useState(null);
