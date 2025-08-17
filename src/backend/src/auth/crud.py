54c4a446d93fca41e9812ee4d5820b27306fcc53
Diy-101 <thehorsediy101@gmail.com>
2025-08-17 18:29:31 +0300

    change session into async_session

--- src/backend/src/auth/crud.py
+++ src/backend/src/auth/crud.py
@@ -1,16 +1,18 @@
-from sqlalchemy.orm import Session
-from sqlalchemy import select
-from src.auth.models import User
-from aiogram.utils.web_app import WebAppUser
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, insert
 
-def get_user(db: Session, user_id: int) -> User:
-    user = db.execute(
-        select(User).where(User.tg_id == user_id)
-    ).scalar_one_or_none()
+from src.auth.models import UserModel
+from src.auth.schemas import UserSchema
+
+async def get_user(session: AsyncSession, user_id: int) -> UserModel:
+    user = await session.execute(
+        select(UserModel).where(UserModel.tg_id == user_id)
+    )
+    user = user.scalar_one_or_none()
     return user
 
-def create_user(db: Session, user_data: WebAppUser) -> User:
-    new_user = User(
+async def create_user(session: AsyncSession, user_data: UserSchema) -> UserSchema:
+    new_user = UserModel(
         tg_id=user_data.id,
         is_bot=user_data.is_bot,
         first_name=user_data.first_name,
@@ -22,7 +24,8 @@ def create_user(db: Session, user_data: WebAppUser) -> User:
         allows_write_to_pm=user_data.allows_write_to_pm,
         photo_url=user_data.photo_url,
     )
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+    stmt = insert(UserModel).values([new_user])
+    await session.execute(stmt)
+    await session.commit()
+    await session.refresh(new_user)
+    return user_data
