fcb2cfe9500a2c15417e6776f6891efcb080314d
Diy-101 <thehorsediy101@gmail.com>
2025-08-23 13:07:57 +0300

    change from sync database engine to async

--- src/backend/src/database.py
+++ src/backend/src/database.py
@@ -1,22 +1,20 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.ext.asyncio import (
-    AsyncSession,
-    create_async_engine,
-    async_sessionmaker
-)
 
 from src.settings import get_settings
 
 cfg = get_settings()
 
 engine = create_async_engine(cfg.database_url)
-SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+create_session = async_sessionmaker(
+    autocommit=False, autoflush=False, bind=engine, class_=AsyncSession
+)
 
 
 async def init_models():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
+
 class Base(DeclarativeBase):
     pass
-
