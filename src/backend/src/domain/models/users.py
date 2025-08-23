71aab175c216b708aaafa3a86eab9bc47e5ef823
Diy-101 <thehorsediy101@gmail.com>
2025-08-23 12:53:26 +0300

    add users sqlalchemy model

---
+++ src/backend/src/domain/models/users.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database import Base
+
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, unique=True, index=True, nullable=False)
+    is_bot: Mapped[bool | None] = None
+    first_name: Mapped[str]
+    last_name: Mapped[str | None] = None
+    username: Mapped[str | None] = None
+    language_code: Mapped[str | None] = None
+    is_premium: Mapped[bool | None] = None
+    allows_write_to_pm: Mapped[bool | None] = None
+    added_to_attachment_menu: Mapped[bool | None] = None
+    photo_url: Mapped[str | None] = None
+
