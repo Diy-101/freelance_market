79c87ae331fa909d7ca0490adc23bac996f14b01
Diy-101 <thehorsediy101@gmail.com>
2025-08-25 19:54:08 +0300

    change field id to tg_id

--- src/backend/src/presentation/fastapi_routers/users.py
+++ src/backend/src/presentation/fastapi_routers/users.py
@@ -20,10 +20,10 @@ async def login(
         raise HTTPException(401, detail=f"Error during initData parsing: {e}")
 
     new_user = init_data.user
-    user = await user_service.get_user(new_user.id)
+    user = await user_service.get_user(new_user.tg_id)
     if user is None:
         user = await user_service.create_user(new_user)
-        token = user_service.create_token(new_user.id)
+        token = user_service.create_token(new_user.tg_id)
         return LoginResponse(
             user=user,
             access_token=token,
@@ -31,7 +31,7 @@ async def login(
     user = await user_service.update_user(user.id, new_user)
     if user is None:
         raise HTTPException(404, detail="User not found in database")
-    token = user_service.create_token(user.id)
+    token = user_service.create_token(user.tg_id)
     return LoginResponse(
         user=user,
         access_token=token,
