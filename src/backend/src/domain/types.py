d7a3c820996d5880820f2127b27a2025d881e96d
Diy-101 <thehorsediy101@gmail.com>
2025-09-03 23:25:18 +0300

    delete generic type for pydantic models

--- src/backend/src/domain/types.py
+++ src/backend/src/domain/types.py
@@ -5,6 +5,3 @@ ModelT = TypeVar("ModelT")
 
 # Универсальный тип для работы с domain entities
 EntityT = TypeVar("EntityT")
-
-# Универсальный тип для работы со схемами (Pydantic BaseModel)
-SchemaT = TypeVar("SchemaT")
