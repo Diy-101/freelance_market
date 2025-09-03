e1de906ac37e9dc557d12fb7975035d73d087bbb
Diy-101 <thehorsediy101@gmail.com>
2025-09-03 20:32:55 +0300

    create order entity

---
+++ src/backend/src/domain/entities/order.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass, field
+from uuid import uuid4
+
+from dataclasses_json import dataclass_json
+
+from src.domain.value_objects import OrderStatus, Skill
+
+
+@dataclass_json
+@dataclass
+class Order:
+    title: str
+    description: str
+    author_id: str
+    status: OrderStatus
+    primary_responses: int
+    skills: list[Skill] = field(default_factory=list)
+    uuid: str = field(default_factory=lambda: str(uuid4()), init=False)
+
+    def change_status(self, new_status: OrderStatus):
+        if not self.status.can_transition_to(new_status):
+            raise ValueError(
+                f"It is impossible to change {self.status} to {new_status}"
+            )
+        self.status = new_status
