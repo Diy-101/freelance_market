56d592c21c0606eb54dddff78aefeaa6145b9661
Diy-101 <thehorsediy101@gmail.com>
2025-09-04 20:22:47 +0300

    change the logic in /login router

--- src/backend/src/presentation/fastapi_routers/users.py
+++ src/backend/src/presentation/fastapi_routers/users.py
@@ -3,6 +3,10 @@ from fastapi import APIRouter, Body, Depends, HTTPException, status
 from src.application.services import TelegramService, UserService
 from src.dependencies import get_telegram_service, get_user_service
 from src.presentation.dto import LoginResponse
+from src.settings import get_settings
+from src.utils import logger
+
+cfg = get_settings()
 
 user_router = APIRouter(
     prefix="/api/users",
@@ -12,20 +16,28 @@ user_router = APIRouter(
 
 @user_router.post("/login")
 async def login(
-    data_string: str = Body(embed=False),
+    data_string: str = Body(embed=True),
     user_service: UserService = Depends(get_user_service),
     telegram_service: TelegramService = Depends(get_telegram_service),
 ) -> LoginResponse:
+    if cfg.debug:
+        logger.debug(data_string)
+
+    # Validating initData from Telegram API
     try:
         init_data = telegram_service.validate_init_data(data_string)
     except Exception as e:
         raise HTTPException(401, detail=f"Error during initData parsing: {e}")
 
-    user = init_data.user
-    is_user_exist = True if await user_service.get_user_by_tg_id(user.tg_id) else False
+    # Check the user existence
+    user_dict = init_data["user"]
+    is_user_exist = (
+        True if await user_service.get_user_by_tg_id(user_dict["tg_id"]) else False
+    )
 
+    # Creates a new user
     if not is_user_exist:
-        user = await user_service.create_user(user)
+        user = await user_service.create_user(user_dict)
         try:
             token = user_service.create_token(user.tg_id)
         except Exception as e:
@@ -33,13 +45,11 @@ async def login(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail=f"Impossible to create an access token: {e}",
             )
-
-        return LoginResponse(
-            user=user,
-            access_token=token,
-        )
+    # Update the user that exists
     else:
-        user = await user_service.update_user_by_tg_id(int(user.tg_id), user)
+        user = await user_service.update_user_by_tg_id(
+            int(user_dict["tg_id"]), user_dict
+        )
         try:
             token = user_service.create_token(user.tg_id)
         except Exception as e:
@@ -48,7 +58,13 @@ async def login(
                 detail=f"Impossible to create an access token: {e}",
             )
 
-        return LoginResponse(
-            user=user,
-            access_token=token,
-        )
+    # Form a response body
+    response = LoginResponse(
+        user=user,
+        access_token=token,
+    )
+
+    if cfg.debug:
+        logger.debug(f"Response: {response}")
+
+    return response
