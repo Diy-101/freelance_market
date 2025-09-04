c9cb4c00ea4885a7cd280c89a5a6154f417a6bb0
Diy-101 <thehorsediy101@gmail.com>
2025-09-04 20:19:14 +0300

    add debug level to logger

--- src/backend/src/utils/logger.py
+++ src/backend/src/utils/logger.py
@@ -1,3 +1,9 @@
 import logging
 
-logger = logging.getLogger("uvicorn")
+from src.settings import get_settings
+
+cfg = get_settings()
+logger = logging.getLogger("uvicorn.error")
+
+if cfg.debug:
+    logger.setLevel(logging.DEBUG)
