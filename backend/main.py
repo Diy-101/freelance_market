1a1429edaa39f2f2592c547fa29e4351873c902f
Diy-101 <thehorsediy101@gmail.com>
2025-09-16 18:36:07 +0300

    delete env

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from src.bot import start_telegram
 from src.database import init_models
-from src.presentation.fastapi_routers.orders import order_router
-from src.presentation.fastapi_routers.users import user_router
+from src.user_service.api.users import user_router
 from src.utils.logger import logger
 from src.webhook import webhook_router
 
@@ -23,7 +22,6 @@ async def lifespan(application: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(webhook_router)
 app.include_router(user_router)
-app.include_router(order_router)
 
 app.add_middleware(
     CORSMiddleware,
