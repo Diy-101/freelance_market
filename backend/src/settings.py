9e536f8c5afcbbcfdcef70b3a4b41170181c056a
Diy-101 <thehorsediy101@gmail.com>
2025-09-17 22:55:16 +0300

    change env

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -1,25 +1,27 @@
 from functools import lru_cache
+from os import getenv
 
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
-    bot_token: str
-    webhook_path: str
-    webhook_url: str
-    debug: bool
-    my_telegram_token: str
-    database_url: str
-    jwt_secret_key: str
-    jwt_algorithm: str
-    jwt_adapter: str
-    mock_user: str
-    model_config = SettingsConfigDict(env_file="../.env", env_file_encoding="utf-8")
+    bot_token = getenv("BOT_TOKEN")
+    webhook_path = getenv("WEBHOOK_PATH")
+    webhook_url = getenv("WEBHOOK_URL")
+    my_telegram_token = getenv("MY_TELEGRAM_TOKEN")
+    database_url = getenv("DATABASE_URL")
+    jwt_secret_key = getenv("JWT_SECRET_KEY")
+    jwt_algorithm = getenv("JWT_ALGORITHM")
+    jwt_adapter = getenv("JWT_ADAPTER")
+    debug = getenv("DEBUG")
 
 
 @lru_cache
 def get_settings() -> Settings:
     """
-    Получение настроек приложения
+    Get app settings
     """
     return Settings()  # type: ignore
