9418752366d4fcde68a780597d1f498ccb24417c
Diy-101 <thehorsediy101@gmail.com>
2025-09-17 18:35:26 +0300

    change host for production

--- backend/main.py
+++ backend/main.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
 
     uvicorn.run(
         app="main:app",
-        host="localhost",
+        host="127.0.0.1",
         port=8000,
         reload=True,
         log_level="debug",
