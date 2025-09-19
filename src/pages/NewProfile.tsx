974b80beddb5834625ccd6d98b43d8df4a74bff4
Diy-101 <thehorsediy101@gmail.com>
2025-09-20 01:58:06 +0300

    change profile

--- src/pages/NewProfile.tsx
+++ src/pages/NewProfile.tsx
@@ -1,104 +1,87 @@
-import { useState } from "react";
-import {
-  Badge,
-  Avatar,
-  Card,
-  CardHeader,
-  CardBody,
-  ButtonGroup,
-  Button,
-} from "@heroui/react";
+import { useEffect, useState } from "react";
+import { Avatar, Card, CardHeader, CardBody } from "@heroui/react";
 import { TabNavigation } from "@/components/TabNavigation";
 import {
   PencilSimpleLineIcon,
   UserIcon,
   TelegramLogoIcon,
-  AddressBookIcon,
   CloverIcon,
 } from "@phosphor-icons/react";
+import useAuth from "@/hooks/useAuth";
 
 export default function Profile() {
-  const [roleTab, setRoleTab] = useState(false);
-
-  const onClickWorker = () => {
-    setRoleTab(false);
-  };
-  const onClickSeller = () => {
-    setRoleTab(true);
-  };
+  const { user } = useAuth();
 
   return (
-    <div className="flex flex-col items-center min-h-screen w-screen pt-10 gap-6">
-      {/* Header */}
-      <div>
-        <span className="font-bold text-2xl">Profile</span>
-      </div>
-      <div className="relative">
-        <Badge
-          isOneChar
-          showOutline={false}
-          className="absolute bg-primary flex place-content-center h-12 w-12 rounded -bottom-8 -right-3"
-          placement="top-right"
-          content={<PencilSimpleLineIcon size={25} className="text-white" />}
-        >
-          <Avatar className="w-36 h-36 border-5" />
-        </Badge>
-      </div>
+    <div className="min-h-screen mx-4">
+      <div className="flex flex-col items-center pt-10 gap-6">
+        {/* Header */}
+        <div>
+          <span className="font-bold text-large">Profile</span>
+        </div>
+        <div className="relative">
+          {user?.photo_url ? (
+            <img
+              src={user?.photo_url}
+              alt={user?.firstName}
+              className={`w-32 h-32 object-cover rounded-full border-5  ${
+                location.pathname == "/profile"
+                  ? "border-primary"
+                  : "border-gray-300"
+              }`}
+            />
+          ) : (
+            <Avatar className="w-32 h-32 border-5 text-gray-500" />
+          )}
 
-      {/* Personal info */}
-      <div className="w-sm bg-white rounded-xl shadow-md/8">
-        <Card className="flex flex-col p-5 gap-6">
-          <CardHeader className="flex justify-between items-end p-0">
-            <p className="font-semibold text-2xl">Personal info</p>
-            <span className="font-bold text-md">Edit</span>
-          </CardHeader>
-          <CardBody className="flex flex-col p-0 gap-2">
-            <div className="flex justify-start gap-4 items-center">
-              <UserIcon size={28} />
-              <div className="flex flex-col">
-                <span className="text-md text-gray-400">Name</span>
-                <p className="text-xl">Ivan Romanov</p>
+          <button className="cursor-pointer absolute bg-primary flex p-2 rounded -bottom-2 -right-2">
+            <PencilSimpleLineIcon size={25} className="text-white" />
+          </button>
+        </div>
+
+        {/* Personal info */}
+
+        <div className="max-w-sm min-w-3xs w-full mx-auto p-4 sm:p-6 bg-white rounded-md shadow-md/8">
+          <Card className="flex flex-col gap-4 rounded-t-none">
+            <CardHeader className="flex justify-between items-center p-0">
+              <p className="font-semibold text-md">Personal info</p>
+              <button className="rounded-md cursor-pointer px-2 py-1 font-semibold text-sm text-white bg-primary">
+                Edit
+              </button>
+            </CardHeader>
+            <CardBody className="flex flex-col p-0 gap-2">
+              <div className="flex justify-start gap-4 items-center">
+                <UserIcon size={24} />
+                <div className="flex flex-col">
+                  <span className="text-sm text-gray-400">Name</span>
+                  <p className="text-md">
+                    {user?.lastName
+                      ? `${user.firstName} ${user.lastName}`
+                      : user?.firstName || "Гость"}
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="flex justify-start gap-4 items-center">
-              <TelegramLogoIcon size={28} />
-              <div className="flex flex-col">
-                <span className="text-md text-gray-400">Username</span>
-                <p className="text-xl">@username</p>
+              <div className="flex justify-start gap-4 items-center">
+                <TelegramLogoIcon size={24} />
+                <div className="flex flex-col">
+                  <span className="text-sm text-gray-400">Username</span>
+                  <p className="text-md">
+                    {user?.userName ? `@${user?.userName}` : "@Гость"}
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="flex justify-start gap-4 items-center">
-              <CloverIcon size={28} />
-              <div className="flex flex-col">
-                <span className="text-md text-gray-400">Premium</span>
-                <p className="text-xl">Yes</p>
+              <div className="flex justify-start gap-4 items-center">
+                <CloverIcon size={24} />
+                <div className="flex flex-col">
+                  <span className="text-sm text-gray-400">Premium</span>
+                  <p className="text-md">{user?.is_premium ? "Yes" : "No"}</p>
+                </div>
               </div>
-            </div>
-          </CardBody>
-        </Card>
+            </CardBody>
+          </Card>
+        </div>
+        <TabNavigation />
       </div>
-
-      {/* Worker and seller profiles */}
-      <ButtonGroup className="w-sm justify-evenly bg-white rounded-xl text-xl shadow-md/8">
-        <Button
-          onPress={onClickWorker}
-          className={`flex-1 m-2 rounded-xl font-medium ${
-            roleTab == false ? "bg-primary text-white" : ""
-          }`}
-        >
-          Worker
-        </Button>
-        <Button
-          onPress={onClickSeller}
-          className={`flex-1 m-2 rounded-xl font-medium ${
-            roleTab ? "bg-primary text-white" : ""
-          }`}
-        >
-          Seller
-        </Button>
-      </ButtonGroup>
-
-      <TabNavigation />
     </div>
   );
 }
